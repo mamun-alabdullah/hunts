8d422eca82909beeca4561cafe99cdbbd6a15653
Abdullah Al mamun <aamamun.dev@gmail.com>
2022-07-26 23:05:32 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,272 @@
+new WOW().init();
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+        $(".menu").addClass("neta")
+    } else {
+        $(".menu").removeClass("neta")
+    }
+
+    if ($(window).scrollTop() > 1000) {
+        $(".backtotop").fadeIn()
+    } else {
+        $(".backtotop").fadeOut()
+    }
+})
+
+// $(".backtotop").click(function(){
+//   $("html,body").animate({
+//     scrollTop:0,
+//   })
+// })
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
+
+
+
 $('.banner-slider').slick({
     dots: false,
-    arrows:true,
+    arrows: true,
     infinite: false,
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
-    fade:true,
+    fade: true,
     prevArrow: '<i class="fas fa-arrow-circle-left prev"></i>',
     nextArrow: '<i class="fas fa-arrow-circle-right next"></i>',
-  });
-
-  $('.service-slider').slick({
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                arrows: false,
+                dots: true,
+                fade: false,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+//servicr part jq
+$('.service-slider').slick({
     dots: false,
-    arrows:true,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-chevron-up perv"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-down next"></i>',
     infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
     vertical: true,
-    centerMode:true,
+    centerMode: true,
     centerPadding: "0",
-  });
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                vertical: false,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                vertical: false,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.testimonial-slider').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-chevron-up perv"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-down next"></i>',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    centerMode: true,
+    centerPadding: "0",
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                vertical: false,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                vertical: false,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.testi-text-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.footer-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+$('.venobox').venobox();
+
+$(".testi-text-slider").slick()
